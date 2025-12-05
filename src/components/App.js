2d199067406113aa65b7aa9c5680f2d143a3a23a
Rutika <rutikap72@gmail.com>
2025-12-05 12:19:10 +0530

    RelativeList

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import RelativeList from './RelativeList'
 const App = () => {
   return (
     <div id="main">
+      <RelativeList/>
                {/* Do not remove the main div */}
     </div>
   )
